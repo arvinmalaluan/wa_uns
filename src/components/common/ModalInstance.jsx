de9e4b0163c005132e65cd562443fca3e38b0622
Arvin Malaluan <malaluanofficial7@gmail.com>
2024-08-05 16:17:35 +0800

    all-files-deleted

--- src/components/common/ModalInstance.jsx
+++
@@ -1,8 +0,0 @@
-import { AlertDialog } from "../ui/alert-dialog";
-import React from "react";
-
-const ModalInstance = ({ children, label }) => {
-  return <AlertDialog>{children}</AlertDialog>;
-};
-
-export default ModalInstance;
--- src/layout/BaseLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BaseLayout = ({ children }) => {
-  return <div className="h-screen w-screen overflow-hidden">{children}</div>;
-};
-
-export default BaseLayout;
